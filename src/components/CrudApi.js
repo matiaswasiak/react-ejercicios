00b7635b5839ab6d7d485061fc3fb50da93584fb
matiaswasiak <matiaswasiak@gmail.com>
2022-02-15 23:30:29 -0300

    Curso React: 34. CRUD API: ( 3 / 5 )

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
 
 const CrudApi = () => {
   const [db, setDb] = useState([]);
   const [dataToEdit, setDataToEdit] = useState(null);
+
+  let api = helpHttp();
+  let url = "http://localhost:5000/frameworks";
+
+  useEffect(() => {
+    api.get(url).then((res) => {
+      // console.log(res);
+      if (!res.err) {
+        setDb(res);
+      } else {
+        setDb(null);
+      }
+    });
+  }, []);
 
   const createData = (data) => {
     data.id = Date.now();
@@ -28,7 +43,7 @@ const CrudApi = () => {
   };
   return (
     <div>
-      <h2>CRUD App</h2>
+      <h2>CRUD API</h2>
       <article className="grid-1-2">
         <CrudForm
           createData={createData}
